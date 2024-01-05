04cde178a5bf0f2caaf90bdfb40329eb09fdd6c3
DevOnlyReadThis <jean.baptiste.andre07@gmail.com>
2024-01-05 11:09:40 +0100

    jhgjhg

--- notemail/src/component/User.jsx
+++ notemail/src/component/User.jsx
@@ -1,5 +1,7 @@
 import style from '../css/user.module.css'
 import Mail from "../assets/MailNotif.png"
+import Yes from "../assets/yes.png"
+import No from "../assets/no.png"
 import { Modal } from 'react-responsive-modal';
 import { useState } from 'react';
 
@@ -12,20 +14,30 @@ export const User = () => {
         <>
         
             <div className={style.containerComponent}>
-                <div>
+                <div className={style.logoMail}>
                     <img src={Mail} alt="" />
                 </div>
-                    <p className={style.txtMail}>Vous avez du courrier en attente</p>
-
-                </div> 
+               <p className={style.txtMail}>Vous avez du courrier en attente</p>    
+            </div> 
+            
+            <div className={style.container_receptionner}>
                 <button onClick={onOpenModal} className={style.receptionner}>Receptionner</button> 
+            </div>
       <Modal open={open} onClose={onCloseModal} center>
-        <h2>Simple centered modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-          hendrerit risus, sed porttitor quam.
+
+      <div className={style.modal_container}>
+
+        <p className={style.txt_confirm}>
+          Confirmer la réception <br />
+          du courrier :
         </p>
+
+        <div className={style.yes_no_modal}>
+          <img src={Yes} alt="" />
+          <img src={No} alt="" />
+        </div>
+
+      </div>
       </Modal>
         </>
     )
