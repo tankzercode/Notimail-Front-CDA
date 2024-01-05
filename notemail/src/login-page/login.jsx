27ba6b39829775ed417c0f4716efe4fca004429e
Pierre-Marie Lebas <pierremarielebas1@gmail.com>
2024-01-05 11:17:12 +0100

    Login update

--- notemail/src/login-page/login.jsx
+++ notemail/src/login-page/login.jsx
@@ -6,6 +6,7 @@ import style from './login.module.css'
 
 const Login = () => {
     /* useState pour contenir mes informations pour me connecter */
+    const [table, setTable] = useState(['Entreprise 1', 'Entreprise 2', 'Entreprise 3']);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -13,7 +14,9 @@ const Login = () => {
     const changeUsername = e => {
         /* On actualise le useState grâce à l'event */
         setUsername(e.target.value)
+        console.log(e.target.value)
     }
+
     /* fonction qui gère l'update de mon useState password */
     const changePassword = e => {
         setPassword(e.target.value)
@@ -32,7 +35,13 @@ const Login = () => {
                 <img src={Logo} className={style.logo} />
                 <form onChange={handleSubmit} action="">
                     <div className={style.inputContainer}>
-                        <input value={username} onChange={changeUsername} type="text" placeholder='Username' required />
+                        {/* <input value={username} onChange={changeUsername} type="text" placeholder='Username' required /> */}
+                        <select onChange={changeUsername} name="pets">
+                            <option value="" disabled selected hidden>Entreprise</option>
+                            {table.map((el,index)=>{
+                                return <option key={index} value={el} > {el}</option>
+                            })}
+                        </select>
                         <div className={style.rightPart}>
                             <img src={ArrowDown} />
                         </div>
@@ -41,14 +50,15 @@ const Login = () => {
                         <input value={password} onChange={changePassword} type="password" required />
                         <div className={style.rightPart}>
                             <div className={style.backgroundLock}>
+                            <button type="submit" className={style.backgroundLock}>
                                 <img src={LockLogin} />
+                            </button>
                             </div>
                         </div>
-                 </div>
-                    </form>
-                </div>
-</>
-            
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
 
