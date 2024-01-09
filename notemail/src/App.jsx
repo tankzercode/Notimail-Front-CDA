3bd1b207e9190ea493c0c497f55b2926ebd5100f
Pierre-Marie Lebas <pierremarielebas1@gmail.com>
2024-01-09 12:30:38 +0100

    Admin jsx/css

--- notemail/src/App.jsx
+++ notemail/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Navbar } from './component/Navbar'
 import { User } from './component/User';
 import ExempleContext from './component/context'
+import Admin from './admin-page/admin';
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Login />} ></Route>
+        <Route path='/admin' element={<Admin />}> </Route>
 
         <Route path='/user' element={<User />}> </Route>
       </Routes>
--- notemail/src/admin-page/admin.jsx
+++ notemail/src/admin-page/admin.jsx
@@ -1,2 +1,29 @@
-import Logo from "../assets/.png"
-import '../css/App.css'
+import { IoIosSearch } from "react-icons/io";
+import editIcon from '../assets/edit-icon.png'
+import style from './admin.module.css'
+
+const Admin = () => {
+
+    return (
+        <>
+            <div className={style.searchBarContainer}>
+                <IoIosSearch/>
+                <input className={style.searchBarContainer.input}type="text" placeholder="Rechercher"/>
+            </div>
+            <div className={style.entrepriseListContainer}>
+                <div className={style.entrepriseCard}>
+                    <div className={style.entrepriseInfos}>
+                        <h3>Entreprise 1</h3>
+                        <p>Nom contact</p>
+                        <p>19/09/2023</p>
+                    </div>
+                    <div className={style.entrepriseEdit}>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Admin
