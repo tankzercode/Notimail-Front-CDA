3abc84cb8f67b58408c262c7c55e2c8011cb5349
DevOnlyReadThis <jean.baptiste.andre07@gmail.com>
2024-01-12 11:05:53 +0100

    cseffds

--- notemail/src/admin-page/admin.jsx
+++ notemail/src/admin-page/admin.jsx
@@ -1,78 +1,45 @@
-import { useEffect } from "react";
-import { Card } from "../component/card";
-import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 import { RiMailSendLine } from "react-icons/ri";
 import { IoMdAdd } from "react-icons/io";
 import style from './admin.module.css'
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Card } from "../component/card";
 
 
-const EntrepriseMoreDetails = () => {
-    const [notifList, setNotifList] = useState([]);
-
-    return (
-        <div className={style.moreDetails}>
-            <div className={style.infosContainer}>
-                <div className={style.infoTitle}>Email:</div>
-                <div>adresse-email@exemple.com</div>
-            </div>
-            <div className={style.infosContainer}>
-                <div className={style.infoTitle}>Téléphone:</div>
-                <div>0123456789</div>
-            </div>
-            <div className={style.infosContainer}>
-                <div className={style.infoTitle}>Identifiant:</div>
-                <div>1337</div>
-            </div>
-        </div>
-
-    );
-};
-
-
-const addNotifEntreprise = () => {
-
-}
 
 const Admin = () => {
+const [users ,setUsers] = useState([])
+    useEffect(()=> {
 
-    const [list, setList] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost:3000/user', {
-            credentials: 'include'
-        }).then((res) => { return res.json() }).then((resp) => {
+        fetch('http://localhost:3000/user').then((res)=>{ return res.json()})
+        .then((resp)=> {
             console.log(resp)
-            setList(resp)
-        }).catch((err) => { console.log(err) })
+            setUsers(resp)
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+
+
     }, [])
+    const [showDetails, setShowDetails] = useState(false)
+  const navigate =useNavigate()
+    const handleCardClick = () => {
+        setShowDetails(!showDetails);
+    };
+
     return (
         <>
-            <div className={style.searchBarContainer}>
-                <IoIosSearch />
-                <input className={style.searchBarContainer.input} type="text" placeholder="Rechercher" />
-            </div>
-            <div className={style.entrepriseListContainer}>
-                <div className={style.moreDetailsContainer} onClick={handleCardClick}>
-                    <div className={style.entrepriseCard}>
-                        <div className={style.entrepriseInfos}>
-                            <h3>Entreprise 1</h3>
-                            <p>Nom contact</p>
-                            <p>19/09/2023</p>
-                        </div>
-                        <div className={style.entrepriseEdit}>
-                            <label className={style.toggleButton}>
-                                <input type="checkbox" />
-                                <span className={style.knob}></span>
-                            </label>
-                            <button>
-                                <FaEdit style={{ color: 'var(--color2)' }} className={style.editButton} />
-                            </button>
-                        </div>
-                    </div>
-                    {showDetails && <EntrepriseMoreDetails></EntrepriseMoreDetails>}
-                </div>
+    <div style={{marginTop:"25px"}}>
+        {users.map((el, index)=>{
+
+            return <div style={{marginBottom:"25px"}}  key={'user'+index}>
+                
+                <Card items={el} ></Card>
+          </div>
+        })}
             </div>
         </>
     );
---
+++ notemail/src/admin-page/editEntreprise.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+
+export const EditEntreprise = ({location}) => {
+
+    console.log(location)
+    return (
+       
+
+
+<div></div>    )
+
+} 
